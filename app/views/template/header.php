1b20198e5d2e7b8cfe4ceb0737b7af7baade4ad0
Bl0nd <fernanda.v.loka@gmail.com>
2025-10-18 16:31:02 -0300

    ;;;;;;;;

--- app/views/template/header.php
+++ app/views/template/header.php
@@ -1,5 +1,5 @@
 <header class="header">
-    <a href="#"><img src="<?= FOTO_BASE; ?>assets/img/logo/logo_torre-sem_fundo.2.png" alt="Torre Forte Logo" width="140px" height="140px" style="filter: invert(39%) sepia(3%) saturate(261%) hue-rotate(162deg) brightness(92%) contrast(87%);"></a>
+    <a href="#"><img src="<?= URL_BASE; ?>assets/img/logoTorre_transp.png" alt="Torre Forte Logo" width="140px" height="140px"></a>
 
     <!-- <button class="fonte-menor" onclick="diminuirFonte()" tabindex="0" data-tts="Diminuir Fonte"> - </button> -->
     <!-- <button class="fonte-maior" onclick="aumentarFonte()" tabindex="0" data-tts="Aumentar Fonte"> + </button> -->
